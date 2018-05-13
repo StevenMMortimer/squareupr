a79ad6c3983a76cdb8f382aa8d55f8ffddb910b2
Steven Mortimer <reportmort@gmail.com>
2018-05-13 00:01:53 -0400

    Fix Broken Customer Test Case

--- tests/testthat/test-customers.R
+++ tests/testthat/test-customers.R
@@ -3,8 +3,7 @@ context("v2 customers endpoint")
 squareupr_test_settings <- readRDS("squareupr_test_settings.rds")
 sq_auth(squareupr_test_settings$personal_access_token)
 
-req_customer_columns <- c("id", "created_at", "updated_at", "given_name", 
-                          "family_name", "preferences", "groups")
+req_customer_columns <- c("id", "created_at", "updated_at", "preferences")
 customers <- sq_list_customers()
 
 test_that("sq_list_customers", {
@@ -17,7 +16,7 @@ test_that("sq_list_customers", {
 test_that("sq_get_customer", {
   
   this_customer <- sq_get_customer(customers$id[1])
-  expect_is(customers, "tbl_df")
+  expect_is(this_customer, "tbl_df")
   expect_true(all(req_customer_columns %in% names(this_customer)))
 
 })
