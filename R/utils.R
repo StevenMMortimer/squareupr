e137f28464396d0c64fc4bedafa96e7d716564b4
Steven Mortimer <reportmort@gmail.com>
2018-05-13 22:32:01 -0400

    Add Extract Customer Groups Function

--- R/utils.R
+++ R/utils.R
@@ -39,3 +39,38 @@ get_os <- function(){
   }
   unname(tolower(os))
 }
+
+#' Extract Group Membership from Customer Data
+#' 
+#' This function reformats the groups field of a list of customers longways (i.e. 
+#' one row per customer per group).
+#'
+#' @importFrom dplyr mutate filter_ select_ as_tibble
+#' @importFrom tidyr unnest_
+#' @importFrom purrr transpose map_df
+#' @param customer_data \code{tbl_df} or \code{data.frame} containing an "id" and 
+#' "groups" field
+#' @return a \code{tbl_df} of customers and their groups
+#' @examples
+#' \dontrun{
+#' our_customers <- sq_list_customers()
+#' cust_groups <- sq_extract_cust_groups(our_customers)
+#' }
+#' @export
+sq_extract_cust_groups <- function(customer_data){
+  stopifnot(all(c("id", "groups") %in% names(customer_data)))
+  
+  res <- customer_data %>%
+    select_("id", "groups") %>%
+    # drop the customers with NULL groups field
+    mutate(groups_cnt = sapply(customer_data$groups, length)) %>%
+    filter_(.dots = "groups_cnt > 0") %>%
+    select_("id", "groups") %>%
+    unnest_("groups") %>%
+    transpose() %>%
+    # convert the groups id and name into two separate columns
+    map_df(~as_tibble(t(unlist(.))))
+  
+  return(res)
+}
+
